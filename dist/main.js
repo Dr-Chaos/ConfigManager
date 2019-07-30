05b5c876deea74a2a1d4a6ccb2d60bb6cc0d50d8
Dr-Chaos <crossazriel@gmail.com>
2019-07-30 20:28:56 +0200

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.createAndLoad = createAndLoad;
 exports.create = create;
 exports.load = load;
 exports.save = save;
 exports.exist = exist;
-exports.createAndLoad = createAndLoad;
-
-var childProcess = _interopRequireWildcard(require("child_process"));
 
 var fs = _interopRequireWildcard(require("fs"));
 
@@ -18,10 +16,24 @@ var path = _interopRequireWildcard(require("path"));
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 /**
+ * create and load json file
+ * @param {String} filePath file path
+ * @returns {JSON} file content
+ */
+function createAndLoad(filePath) {
+  if (!exist(filePath)) {
+    create(filePath);
+  }
+
+  return load(filePath);
+}
+/**
  * create json file
  * @param {String} filePath file path
  * @returns {Boolean} boolean
  */
+
+
 function create(filePath) {
   if (createDir(filePath)) {
     try {
@@ -62,20 +74,6 @@ function save(filePath, content) {
 
 function exist(filePath) {
   return fs.existsSync(filePath);
-}
-/**
- * create and load json file
- * @param {String} filePath file path
- * @returns {JSON} file content
- */
-
-
-function createAndLoad(filePath) {
-  if (!exist(filePath)) {
-    create(filePath);
-  }
-
-  return load(filePath);
 }
 /**
  * create file directory
